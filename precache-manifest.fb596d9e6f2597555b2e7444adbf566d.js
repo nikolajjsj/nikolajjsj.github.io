8ee85f403fb1a13d4e816c5c148d9f744074c87b
nikolajjsj <nikolaj.app.dev@gmail.com>
2020-09-21 11:38:05 +0200

    Ported my netlify website to also run here

---
+++ precache-manifest.fb596d9e6f2597555b2e7444adbf566d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6195461f24aa5d8e329942a23c45ddd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b01205eff52a4fc1a6d",
+    "url": "/static/css/main.062ba85d.chunk.css"
+  },
+  {
+    "revision": "df4ba25e4e65be4b5e6e",
+    "url": "/static/js/2.8684a6bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8684a6bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b01205eff52a4fc1a6d",
+    "url": "/static/js/main.c128d8a4.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "f63ace7baa462c8b46af4f1318817ca6",
+    "url": "/static/media/bachelor.f63ace7b.png"
+  },
+  {
+    "revision": "f25201ec5ec75ac6b02614f654b828a0",
+    "url": "/static/media/headshot.f25201ec.jpg"
+  },
+  {
+    "revision": "163a83b7311a06fd202ef4c3d6dc2cef",
+    "url": "/static/media/pathfinder.163a83b7.png"
+  },
+  {
+    "revision": "f2e48167ebc068472bc986a26d2f50ed",
+    "url": "/static/media/pexels0.f2e48167.png"
+  },
+  {
+    "revision": "b2fbe26511d1ba9e238e2561ae4b6411",
+    "url": "/static/media/physiology0.b2fbe265.png"
+  },
+  {
+    "revision": "09c297e43ec851730a356f9e577f961b",
+    "url": "/static/media/reddit0.09c297e4.png"
+  },
+  {
+    "revision": "1e57015beb3b3b49be49e52e5848a55b",
+    "url": "/static/media/sorting.1e57015b.png"
+  },
+  {
+    "revision": "25582e8835a3809638c759914a16c753",
+    "url": "/static/media/sygeplej0.25582e88.png"
+  },
+  {
+    "revision": "4c130adfcb092691ca27791f75942286",
+    "url": "/static/media/tictactoe.4c130adf.png"
+  }
+]);
